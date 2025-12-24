e96c4baa9aba06ac05de92c1603484cbcd85ce50
Hala Chafik <hala.chafik@student-cs.fr>
2025-12-25 00:18:16 +0100

    feat: add __init__ files

---
+++ src/dtw_gi/__init__.py
@@ -0,0 +1,8 @@
+from .bcd import dtw_gi_bcd_stiefel, dtw_gi_bcd_stiefel_multistart
+from .stiefel import stiefel_procrustes_update
+
+__all__ = [
+    "dtw_gi_bcd_stiefel",
+    "dtw_gi_bcd_stiefel_multistart",
+    "stiefel_procrustes_update",
+]
---
+++ src/dtw_gi/dtw/__init__.py
@@ -0,0 +1,5 @@
+from .dtw import dtw
+from .alignment import path_to_W
+from .backend import compute_dtw_path
+
+__all__ = ["dtw", "path_to_W", "compute_dtw_path"]
